cc671a2db7415777a7c7fdc48b6452e6599eb868
GeoffreyLong <GLong14@GMail.com>
2013-10-06 06:58:49 -0400

    was trying to pass the class with a tuple (), problem fixed

--- TimeOut.py
+++ TimeOut.py
@@ -13,18 +13,18 @@ class timer:
         self.accel = Accelerometer.arduino(self.workout, self.weight, True)
         
     def start(self):
-        self.accel().start()
+        self.accel.start()
 
     def stop(self):
         self.accel.stop()
-        
+'''       
     def makeGraph (avgPowers):                                                   # First import pygal
-	bar_chart = pygal.Bar()                                            # Makes a simple bar graph of powers eg: avgPowers = [5.2, 2.5, 7.9]
-	bar_chart.add('Power (in J)', avgPowers)  
-	bar_chart.x_labels = map(str, range(1, len(avgPowers)+1))
-	bar_chart.render_to_file('Power_bar_chart.svg')
-	os.system("start "+'Power_bar_chart.svg')
-
+    bar_chart = pygal.Bar()                                            # Makes a simple bar graph of powers eg: avgPowers = [5.2, 2.5, 7.9]
+    bar_chart.add('Power (in J)', avgPowers)  
+    bar_chart.x_labels = map(str, range(1, len(avgPowers)+1))
+    bar_chart.render_to_file('Power_bar_chart.svg')
+    os.system("start "+'Power_bar_chart.svg')
+'''
 # this will stop the timer
 
 
