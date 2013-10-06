b446a066652bdbdc020d15918ae9cab277328e51
GeoffreyLong <GLong14@GMail.com>
2013-10-06 04:19:02 -0400

    Fixed the styling of the elements

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
 from Tkinter import *
-
+import tkFont
 
 
 class Example(Frame):
@@ -18,18 +18,43 @@ def initializeNext():
 
 def main():
     root.geometry("300x300+300+40")
+    
+    titleFont = tkFont.Font(family="Helvetica",size=14,weight="bold")
+    title = Label(root, text="WORKOUT SELECTION", font=titleFont)
+    title.place(x=45, y=20)
+    
+    liftLabel = Label(root, text="Enter the workout : ")
+    liftLabel.place(x=5, y=55)
+    
     optionList = ("bench", "curls", "delt raises")
     optionVar.set(optionList[0])
     option = OptionMenu(root, optionVar, *optionList)
-    b = Button(root, text="Start the workout", command=initializeNext)
-    option.pack()
-    text.pack()    
-    b.pack()
+    option.place(x=190, y=50)
+    
+    weightLabel = Label(root, text="Enter the mass of weights (Kg) : ")
+    weightLabel.place(x=5, y=100)
+    
+    text.place(x=210, y=100)  
+    
+    lineBreak = Label(root, text="_____________________________________________")
+    lineBreak.place(x=12, y=160)
+    
+    note = Label(root, text="please rest the odometer in")
+    note.place(x=55, y=180)
+    noteContOne = Label(root, text="the starting position for at least one second")
+    noteContOne.place(x=10, y=200)
+    noteContTwo = Label(root, text="to initiate the accelerometer")
+    noteContTwo.place(x=55, y=220)
+    
+    b = Button(root, text="Start the workout", command=initializeNext, background="#6AA5F2", 
+               activebackground="#6AD5F2", border=3)
+    b.place(x=75, y=250)
+    
     root.mainloop()  
     
 #officeresquire
 root = Tk()
-text = Entry(root)
+text = Entry(root, width=8)
 optionVar = StringVar(root)
 if __name__ == '__main__':
     main() 
